4252afc6a3f8f0fabb7045c4c1428622b522f5f5
KUNGPOTATO\kungp <kittisak.py@gmail.com>
2023-08-09 21:46:34 +0700

    g

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,30 +2,15 @@
 
 namespace Database\Seeders;
 
-// database/seeders/UsersTableSeeder.php
-
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        $users = [
-            [
-                'name' => Str::random(10),
-                'email' => Str::random(10) . '@gmail.com',
-                'password' => Hash::make('password'),
-                'email_verified_at' => Carbon::now(),
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ],
-            // Add more users as needed...
-        ];
-
-        User::insert($users);
+        User::factory()
+            ->count(10)
+            ->create();
     }
 }
